3f1b6aabf34b155848635341056eb7e7c4d69ff5
Rakhat <85451882+Rakhat03@users.noreply.github.com>
2023-10-16 23:43:56 +0600

    Add files via upload

---
+++ simple_facerec.py
@@ -0,0 +1,63 @@
+import face_recognition
+import cv2
+import os
+import glob
+import numpy as np
+
+class SimpleFacerec:
+    def __init__(self):
+        self.known_face_encodings = []
+        self.known_face_names = []
+
+        # Resize frame for a faster speed
+        self.frame_resizing = 0.25
+
+    def load_encoding_images(self, images_path):
+        """
+        Load encoding images from path
+        :param images_path:
+        :return:
+        """
+        # Load Images
+        images_path = glob.glob(os.path.join(images_path, "*.*"))
+        
+        #print("IMG_PATH:",images_path)#
+        
+        #print("{} encoding images found.".format(len(images_path)))
+
+        # Store image encoding and names
+        for img_path in images_path:
+            img = cv2.imread(img_path)
+            rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+            # Get the filename only from the initial file path.
+            basename = os.path.basename(img_path)
+            
+            #print("Basename:",basename)#
+            
+            (filename, ext) = os.path.splitext(basename)
+            
+            #print("Filename:",filename)#
+            #print("Ext:",ext)#
+            #print("-------------------")#
+            
+            # Get encoding
+            img_encoding = face_recognition.face_encodings(rgb_img)[0]
+
+            # Store file name and file encoding
+            self.known_face_encodings.append(img_encoding)
+            self.known_face_names.append(filename)
+        #print("Encoding images loaded")
+
+    def detect_known_faces(self, frame):
+        
+        matches = face_recognition.compare_faces(self.known_face_encodings, frame)
+        name = "Unknown"
+
+        # If a match was found in known_face_encodings, just use the first one.
+        if True in matches:
+            first_match_index = matches.index(True)
+            name = self.known_face_names[first_match_index]
+        
+        
+        return name
